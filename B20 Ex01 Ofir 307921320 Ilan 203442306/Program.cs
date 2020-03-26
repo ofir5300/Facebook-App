1d001184f8e68c147a71de500113441972b9f53e
Ofir Cohen <ofirch@mta.ac.il>
2020-03-26 16:38:09 +0200

    Add project files.

---
+++ B20 Ex01 Ofir 307921320 Ilan 203442306/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace B20_Ex01_Ofir_307921320_Ilan_203442306
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
